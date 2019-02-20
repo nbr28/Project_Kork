471a2f730cc6a1b8c3bb4874f73634789d2ba79c
Jerry Shueh <shueh.jerry@gmail.com>
2019-02-20 00:36:53 -0500

    Update ClickManager.cs
    Added new selection mode to ClickManager for selecting yarn endpoints.

--- Assets/Scripts/UI/ClickManager.cs
+++ Assets/Scripts/UI/ClickManager.cs
@@ -4,16 +4,34 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityEngine;
 
-public class ClickManager : MonoBehaviour  
+public class ClickManager : MonoBehaviour
 {
     public UIManager uiManager;
     public Button showSnippetContentButton; //There should be a better solution to this, but we'll deal with it later
+    public int clickMode; // 0 = normal selection, 1 = yarn selection
 
     private Transform lastHit;
     private Transform objectHit;
 
+    private void Awake()
+    {
+        clickMode = 0;
+    }
+
     // Update is called once per frame
     void Update()
+    {
+        if (clickMode == 0)
+        {
+            normalClickSelect();
+        }
+        else if (clickMode == 1)
+        {
+            yarnClickSelect();
+        }
+    }
+
+    private void normalClickSelect()
     {
         // Check if the mouse was clicked over a UI element
         if (!EventSystem.current.IsPointerOverGameObject())
@@ -48,6 +66,84 @@ public class ClickManager : MonoBehaviour
                 //If the current hit object is a Snippet
                 if (objectHit.GetComponent<SnippetState>())
                 {
+                    objectHit.GetComponent<SnippetOutline>().setOutlineColor(255, 246, 0, 255);
+                    objectHit.GetComponent<SnippetOutline>().setToggle(true);
+                }
+
+                //If user clicks
+                if (Input.GetMouseButtonDown(0))
+                {
+                    //If the clicked object is a Snippet
+                    if (objectHit.GetComponent<SnippetState>())
+                    {
+                        uiManager.loadSnippetDetails(objectHit.GetComponent<SnippetState>());
+                        uiManager.showSnippetDetails();
+                    }
+                    else
+                    {
+                        uiManager.clearSnippetDetails();
+                        uiManager.closeSnippetDetails();
+                    }
+                }
+            }
+            else
+            {
+                objectHit = null;
+
+                //If we hit nothing, remove highlight on last selected snippet
+                if (lastHit && lastHit.GetComponent<SnippetState>())
+                {
+                    lastHit.GetComponent<SnippetOutline>().setToggle(false);
+                }
+
+                //If user clicks nothing
+                if (Input.GetMouseButtonDown(0))
+                {
+                    uiManager.clearSnippetDetails();
+                    uiManager.closeSnippetDetails();
+                }
+            }
+
+            updateDebugUI();
+        }
+    }
+
+    private void yarnClickSelect()
+    {
+        // Check if the mouse was clicked over a UI element
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            // Generate a ray that travels outward from the mouse click point
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            // If the generated ray hit something, that means it was clicked
+            if (Physics.Raycast(ray, out hit))
+            {
+                //Get the hit object
+                objectHit = hit.transform;
+
+                //Assign last hit object if not assigned yet
+                if (!lastHit)
+                {
+                    lastHit = objectHit;
+                }
+                else if (lastHit != objectHit)
+                {
+                    //If we hit a different object than the last, remove hightlight on last selected snippet
+                    if (lastHit.GetComponent<SnippetState>())
+                    {
+                        lastHit.GetComponent<SnippetOutline>().setToggle(false);
+                    }
+
+                    //Assign new last hit object
+                    lastHit = objectHit;
+                }
+
+                //If the current hit object is a Snippet
+                if (objectHit.GetComponent<SnippetState>())
+                {
+                    objectHit.GetComponent<SnippetOutline>().setOutlineColor(255, 0, 0, 255);
                     objectHit.GetComponent<SnippetOutline>().setToggle(true);
                 }
 
@@ -108,6 +204,6 @@ public class ClickManager : MonoBehaviour
         {
             uiManager.setDbgObjectHitText("None");
         }
-        
+
     }
 }
