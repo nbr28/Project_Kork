5912c5eddd3018147b5bb71fdab6e9f6a8b0ceca
nbr28 <natanross@hotmail.com>
2019-02-20 05:04:37 -0500

    removed mono behavior

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -64,8 +64,8 @@ public class UIManager : MonoBehaviour
 
     void Awake()
     {
-        this._YarnHandler = new YarnHandler(this);
-        this._YarnLineHandler = new YarnLineHandler(this);
+        this._YarnHandler = new YarnHandler();
+        this._YarnLineHandler = new YarnLineHandler();
         //dbgPanel.SetActive(false);
         dimmerPanel.gameObject.SetActive(false);
         snippetDetailsPanel.gameObject.SetActive(false);
