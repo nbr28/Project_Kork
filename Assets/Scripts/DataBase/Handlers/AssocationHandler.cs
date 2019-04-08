f11bbb81e5876742befb73ed180efe758180970d
Natan Ross <natanross@hotmail.com>
2019-04-08 13:17:55 -0400

    Removed obsolete functions

--- Assets/Scripts/DataBase/Handlers/AssocationHandler.cs
+++ Assets/Scripts/DataBase/Handlers/AssocationHandler.cs
@@ -11,15 +11,6 @@ namespace Assets.Scripts.DataBase
 {
     public class AssocationHandler : GenericCRUD<Association>
     {
-        [Obsolete]
-        private RequestHelper currentRequest;
-        [Obsolete]
-        private MonoBehaviour mono;
-        [Obsolete]
-        private Association[] associations;
-        [Obsolete]
-        private Association association;
-        [Obsolete]
         private AssociationView[] associationViews;
 
         private const string baseTable = "Association";
@@ -76,90 +67,5 @@ namespace Assets.Scripts.DataBase
         {
             return JsonUtility.FromJson<Association>(base.Put(URLConfig.BASEURL + baseTable, obj));
         }
-
-
-
-
-
-
-
-
-        /// <summary>
-        /// GET request for all associations.
-        /// </summary>
-        /// <param name="uri">url address that will be used for API call</param>
-        [Obsolete]
-        private IEnumerator GetRequestGetAllAssociations(string uri = URLConfig.BASEURL + baseTable + "s")
-        {
-            WWW request = new WWW(uri);
-            while (!request.isDone) ;
-            associations = JsonHelper.getJsonArray<Association>(request.text);
-            yield return request;
-        }
-
-        /// <summary>
-        /// GET request for association with given id.
-        /// </summary>
-        /// <param name="associationId">association id</param>
-        /// <param name="uri">url address that will be used for API call</param>
-        [Obsolete]
-        private IEnumerator GetRequestGetAssociation(int associationId, string uri = URLConfig.BASEURL + baseTable)
-        {
-            WWW request = new WWW(uri + "/" + associationId);
-            while (!request.isDone) ;
-            association = JsonHelper.getJsonArray<Association>(request.text)[0];
-            yield return request;
-        }
-
-        [Obsolete]
-        private IEnumerator GetAssociationViewsForASnippet(int snippetId)
-        {
-            WWW request = new WWW(URLConfig.BASEURL + "AssociationView/" + snippetId);
-            while (!request.isDone) ;
-            associationViews = JsonHelper.getJsonArray<AssociationView>(request.text);
-            yield return request;
-        }
-
-        /// <summary>
-        /// Post request
-        /// </summary>
-        /// <param name="tag">Tag to be sent</param>
-        /// <param name="uri"></param>
-        [Obsolete]
-        private IEnumerator PostRequestAssociation(Association association, string uri = URLConfig.BASEURL + baseTable)
-        {
-            WWWForm form = new WWWForm();
-            form.AddField("Association_Id", association.Association_Id);
-            form.AddField("Association_Name", association.Association_Name);
-            form.AddField("Association_Data_Type", (int) association.Association_Data_Type);
-            using (UnityWebRequest webRequest = UnityWebRequest.Post(uri, form))
-            {
-                while (!webRequest.isDone) ;
-                yield return webRequest;
-            }
-        }
-
-
-        /// <summary>
-        /// Put request by the ID of the association ONLY
-        /// </summary>
-        /// <param name="tag">Association to be sent</param>
-        /// <param name="uri"></param>
-        [Obsolete]
-        private IEnumerator PutRequestAssociation(Association association, string uri = URLConfig.BASEURL + baseTable)
-        {
-            WWWForm form = new WWWForm();
-            form.AddField("Association_Id", association.Association_Id);
-            form.AddField("Association_Name", association.Association_Name);
-            form.AddField("Association_Data_Type", (int)association.Association_Data_Type);
-            using (UnityWebRequest webRequest = UnityWebRequest.Post(uri, form))
-            {
-                webRequest.method = "PUT";//sets method to put not post
-                while (!webRequest.isDone) ;
-                yield return webRequest;
-            }
-        }
-
-
     }
 }
--- Assets/Scripts/DataBase/Handlers/ConnectionHandler.cs
+++ Assets/Scripts/DataBase/Handlers/ConnectionHandler.cs
@@ -11,11 +11,7 @@ namespace Assets.Scripts.DataBase
     {
 
         private const string baseTable = "Connection";
-        [Obsolete]
-        private Connection connection;
 
-        [Obsolete]
-        private Connection[] connections;
 
         /// <summary>
         /// Getter for all connections.
@@ -24,7 +20,7 @@ namespace Assets.Scripts.DataBase
         public Connection[] GetAllConnections()
         {
 
-           // mono.StartCoroutine(GetRequestGetAllConnections());
+            // mono.StartCoroutine(GetRequestGetAllConnections());
             return JsonHelper.getJsonArray<Connection>(base.Get(URLConfig.BASEURL + baseTable + "s"));
         }
 
@@ -83,122 +79,5 @@ namespace Assets.Scripts.DataBase
         {
             return JsonUtility.FromJson<Connection>(base.Put(URLConfig.BASEURL + baseTable, connection));
         }
-
-
-        /// <summary>
-        /// GET request for all connections.
-        /// </summary>
-        /// <param name="uri">url address that will be used for API call</param>
-        [Obsolete]
-        private IEnumerator GetRequestGetAllConnections(string uri = URLConfig.BASEURL + baseTable + "s")
-        {
-            WWW request = new WWW(uri);
-            while (!request.isDone) ;
-            connections = JsonHelper.getJsonArray<Connection>(request.text);
-            yield return request;
-        }
-
-        /// <summary>
-        /// GET request for connection with given snippet id and association id.
-        /// </summary>
-        /// <param name="snippetId">snippet id</param>
-        /// <param name="associationId">association id</param>
-        /// <param name="uri">url address that will be used for API call</param>
-        [Obsolete]
-        private IEnumerator GetRequestGetConnection(int snippetId, int associationId, string uri = URLConfig.BASEURL + baseTable)
-        {
-            WWW request = new WWW(uri + "/single?Snippet_Id=" + snippetId + "&Association_Id=" + associationId);
-            while (!request.isDone) ;
-            connection = JsonHelper.getJsonArray<Connection>(request.text)[0];
-            yield return request;
-        }
-
-        /// <summary>
-        /// GET request for connection with given snippet id.
-        /// </summary>
-        /// <param name="snippetId">snippet id</param>
-        /// <param name="uri">url address that will be used for API call</param>
-        [Obsolete]
-        private IEnumerator GetRequestGetConnectionBySnippetId(int snippetId, string uri = URLConfig.BASEURL + baseTable)
-        {
-            WWW request = new WWW(uri + "/snippet/" + snippetId);
-            while (!request.isDone) ;
-            connection = JsonHelper.getJsonArray<Connection>(request.text)[0];
-            yield return request;
-        }
-
-        /// <summary>
-        /// GET request for connection with given association id.
-        /// </summary>
-        /// <param name="associationId">association id</param>
-        /// <param name="uri">url address that will be used for API call</param>
-        [Obsolete]
-        private IEnumerator GetRequestGetConnectionByAssociationId(int associationId, string uri = URLConfig.BASEURL + baseTable)
-        {
-            WWW request = new WWW(uri + "/association/" + associationId);
-            while (!request.isDone) ;
-            connection = JsonHelper.getJsonArray<Connection>(request.text)[0];
-            yield return request;
-        }
-
-
-        /// <summary>
-        /// Post request
-        /// </summary>
-        /// <param name="tag">Tag to be sent</param>
-        /// <param name="uri"></param>
-        [Obsolete]
-        private IEnumerator PostRequestConnection(Connection connection, string uri = URLConfig.BASEURL + baseTable)
-        {
-            WWWForm form = new WWWForm();
-            form.AddField("Snippet_Id", connection.Snippet_Id);
-            form.AddField("Assoication_Id", connection.Association_Id);
-            form.AddField("Connection_Data", connection.Connection_Data);
-            using (UnityWebRequest webRequest = UnityWebRequest.Post(uri, form))
-            {
-                while (!webRequest.isDone) ;
-                yield return webRequest;
-            }
-        }
-
-        /// <summary>
-        /// Post request
-        /// </summary>
-        /// <param name="tag">Tag to be sent</param>
-        /// <param name="uri"></param>
-        [Obsolete]
-        private void PutRequestConnection(Connection connection, string uri = URLConfig.BASEURL + baseTable)
-        {
-            //WWWForm form = new WWWForm();
-            //form.AddField("Snippet_Id", connection.Snippet_Id);
-            //form.AddField("Assoication_Id", connection.Assoication_Id);
-            //form.AddField("Connection_Data", connection.data);
-            //using (UnityWebRequest webRequest = UnityWebRequest.Post(uri, form))
-            //{
-            //    //webRequest.method = "PUT";
-            //    while (!webRequest.isDone||!webRequest.isNetworkError) ;
-            //    yield return webRequest;
-            //}
-
-
-            //RestClient.Put<Connection>(uri, new Connection
-            //{
-            //    Snippet_Id = connection.Snippet_Id,
-            //    Association_Id = connection.Association_Id,
-            //    data = connection.data
-            //}, (err, res, body) =>
-            //{
-            //    if (err != null)
-            //    {
-            //        // EditorUtility.DisplayDialog("Error", err.Message, "Ok");
-            //    }
-            //    else
-            //    {
-            //        //EditorUtility.DisplayDialog("Success", JsonUtility.ToJson(body, true), "Ok");
-            //    }
-            //});
-        }
-
-
     }
 }
