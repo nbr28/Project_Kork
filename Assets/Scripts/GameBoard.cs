451f52a9c4c4044869e25ea58e522c38af20dc50
ApolllonDev <oleksii.polovyi@gmail.com>
2019-04-11 03:26:00 -0400

    fix colors

--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -10,7 +10,7 @@ public class GameBoard : MonoBehaviour
     void Start()
     {
         //assigns a random color
-        gameObject.GetComponent<Image>().color = Random.ColorHSV(0, 1, 1, 1, 1, 1);
+        // gameObject.GetComponent<Image>().color = Random.ColorHSV(0, 1, 1, 1, 1, 1);
         //randomImage();
     }
 
