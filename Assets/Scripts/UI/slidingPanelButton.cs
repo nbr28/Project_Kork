c544557af8b0225297f0497ee2b65c3bde9795bc
Jerry Shueh <shueh.jerry@gmail.com>
2019-04-11 03:26:55 -0400

    Adjusted starting camera

---
+++ Assets/Scripts/UI/slidingPanelButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using DG.Tweening;
+
+public class slidingPanelButton : MonoBehaviour
+{
+    public RectTransform panel;
+    private bool plusTog;
+
+    public void togglePanel()
+    {
+        if (!plusTog)
+        {
+            panel.DOAnchorPosX(6.0f, -85.0f);
+            plusTog = true;
+        }
+        else
+        {
+            panel.DOAnchorPosX(-161.0f, -85.0f);
+            plusTog = false;
+        }
+    }
+
+}
